32a2b6516b0dfcf84912128f6e2a664a64f57dc0
Dominik Stodolny <d.stodolny@gmail.com>
2015-03-13 02:43:53 +0100

    solve select name exercises

--- tutorial1.sql
+++ tutorial1.sql
@@ -1,39 +1,62 @@
--- SELECT basics
-
--- 1. Show the population of Germany
-
-SELECT population FROM world
-  WHERE name = 'Germany'
+-- SELECT name
 
 
--- 2. Show the name and per capita gdp: gdp/population for each country where the area is over 5,000,000 km2
-
-SELECT name, gdp/population FROM world
-  WHERE area > 5000000
-
-
--- 3. Show the name and continent where the area is less than 2000 and the gdp is more than 5000000000
-
-SELECT name , continent
-  FROM world
-  WHERE area < 2000
-    AND gdp > 5000000000
-
-
--- 4. Show the name and the population for 'Denmark', 'Finland', 'Norway', 'Sweden'
-
-SELECT name, population FROM world
-  WHERE name IN ('Denmark', 'Finland',
-                 'Norway', 'Sweden')
-
-
--- 5. Show each country that begins with G
+-- 1. Find the country that start with Y
 
 SELECT name FROM world
-  WHERE name LIKE 'G%'
+  WHERE name LIKE 'Y%'
 
 
--- 6. Show the area in 1000 square km. Show area/1000 instead of area
+-- 2. Find the countries that end with y
 
-SELECT name, area/1000 FROM world
-  WHERE area/1000 BETWEEN 200 AND 250
+SELECT name FROM world
+  WHERE name LIKE '%y'
+
+
+-- 3. Find the countries that contain the letter x
+
+SELECT name FROM world
+  WHERE name LIKE '%x%'
+
+
+-- 4. Find the countries that end with land
+
+SELECT name FROM world
+  WHERE name LIKE '%land'
+
+
+-- 5. Find the countries that start with C and end with ia
+
+SELECT name FROM world
+  WHERE name LIKE 'C%' AND name LIKE '%ia'
+
+
+-- 6. Find the country that has oo in the name
+
+SELECT name FROM world
+  WHERE name LIKE '%oo%'
+
+
+-- 7. Find the countries that have three or more a in the name
+
+SELECT name FROM world
+  WHERE name LIKE '%a%a%a%'
+
+
+-- 8. Find the countries that have "t" as the second character.
+
+SELECT name FROM world
+ WHERE name LIKE '_t%'
+ORDER BY name
+
+
+-- 9. Find the countries that have two "o" characters separated by two others.
+
+SELECT name FROM world
+ WHERE name LIKE '%o__o%'
+
+
+-- 10. Find the countries that have exactly four characters.
+
+SELECT name FROM world
+ WHERE name LIKE '____'
